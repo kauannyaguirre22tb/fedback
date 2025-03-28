08439b139cf39f25ce990fc1153e00beb9a66a3e
kauanny.aguirre853 <kauanny.aguirre853@gmail.com>
2025-03-28 13:16:12 +0000

    fim do HTML e início das estilizações

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,24 @@ import iconStar from "./assets/icon-star.svg"
 
 export function App() {
   return(
-    <div className="bg-radial-gradient text-white">
-      <div>
-      
+    <div className="bg-radial-gradient text-white rounded-2xl p-6 max-w-103 font-overpass">
+      <div className="bg-dark-blue w-fit p-4 rounded-full mb-4">
+         <img src={iconStar} alt="icon star" /> 
       </div>
+
+      <h1 className="text-2xl font-bold mb-2.5">How did we do?</h1>
+
+      <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+
+      <div>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      </div>
+
+      <button>Submit</button>
     </div>
   )
 }
