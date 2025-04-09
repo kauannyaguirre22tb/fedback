427d8fa4b259e2517fd8d6a0ac830a54d093716d
kauanny.aguirre853 <kauanny.aguirre853@gmail.com>
2025-04-09 15:10:48 +0000

    criação das funções e lógica da avalição dos cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import iconStar from "./assets/icon-star.svg"
+import { useState } from "react"
 
 export function App() {
-  return(
+  const [submited, setSubmited] = useState(false)
+  const [rateNote, setRateNote] = useState(0)
+  
+  function handleSubmit() {
+    if (rateNote !== 0) {
+    setSubmited(true)
+    return
+    }
+
+    alert("Please, choose a rate button!")
+  }
+
+  function handleChangeRateNote(value) {
+    setRateNote(value)
+  }
+ 
+return (
+  submited === false ? (
     <div className="bg-radial-gradient text-white rounded-2xl p-6 max-w-103 font-overpass">
       <div className="bg-dark-blue w-fit p-4 rounded-full mb-4">
          <img src={iconStar} alt="icon star" /> 
@@ -12,14 +30,19 @@ export function App() {
       <p className="text-sm text-light-grey leading-1 mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
 
       <div className="flex justify-between mb-6">
-      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-white">1</button>
-      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-white">2</button>
-      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-white">3</button>
-      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-white">4</button>
-      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-white">5</button>
+      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-whit font-bold" onClick={ () => handleChangeRateNote(1)}>1</button>
+      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-whit font-bold" onClick={ () => handleChangeRateNote(2)}>2</button>
+      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-whit font-bold" onClick={ () => handleChangeRateNote(3)}>3</button>
+      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-whit font-bold" onClick={ () => handleChangeRateNote(4)}>4</button>
+      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-whit font-bold" onClick={ () => handleChangeRateNote(5)}>5</button>
       </div>
 
-      <button className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1">Submit</button>
+      <button onClick={handleSubmit} className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1">Submit</button>
+    </div>
+  ) : (
+    <div className="bg-radial-gradient text-white rounded-2xl p-6 max-w-103 font-overpass">
+      <p>enviou tudo</p>
     </div>
   )
+)
 }
