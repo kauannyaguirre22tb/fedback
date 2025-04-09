1973750ae2054482d2300aa0927519334386365b
kauanny.aguirre853 <kauanny.aguirre853@gmail.com>
2025-04-09 15:41:10 +0000

    ajustes de estilização para a versão desktop e fim do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -31,19 +31,19 @@ return (
       <p className="text-sm text-light-grey leading-1 mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
 
       <div className="flex justify-between mb-6">
-      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-whit font-bold" onClick={ () => handleChangeRateNote(1)}>1</button>
-      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-whit font-bold" onClick={ () => handleChangeRateNote(2)}>2</button>
-      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-whit font-bold" onClick={ () => handleChangeRateNote(3)}>3</button>
-      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-whit font-bold" onClick={ () => handleChangeRateNote(4)}>4</button>
-      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-whit font-bold" onClick={ () => handleChangeRateNote(5)}>5</button>
+      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-whit font-bold hover:bg-orange hover:text-white cursor-pointer duration-400 sm:w-13 sm:h-13" onClick={ () => handleChangeRateNote(1)}>1</button>
+      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-whit font-bold hover:bg-orange hover:text-white cursor-pointer duration-400 sm:w-13 sm:h-13" onClick={ () => handleChangeRateNote(2)}>2</button>
+      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-whit font-bold hover:bg-orange hover:text-white cursor-pointer duration-400 sm:w-13 sm:h-13" onClick={ () => handleChangeRateNote(3)}>3</button>
+      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-whit font-bold hover:bg-orange hover:text-white cursor-pointer duration-400 sm:w-13 sm:h-13" onClick={ () => handleChangeRateNote(4)}>4</button>
+      <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-grey focus:bg-medium-grey focus:text-whit font-bold hover:bg-orange hover:text-white cursor-pointer duration-400 sm:w-13 sm:h-13" onClick={ () => handleChangeRateNote(5)}>5</button>
       </div>
 
-      <button onClick={handleSubmit} className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1">Submit</button>
+      <button onClick={handleSubmit} className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1 hover:bg-white hover:text-orange duration-400 cursor-pointer">Submit</button>
     </div>
   ) : (
     <div className="bg-radial-gradient text-white rounded-2xl p-6 max-w-103 font-overpass text-center">
      <img className="mx-auto mb-6" src={illustrationThankYou} alt="illustration Thank You" />
-     <p className="bg-dark-blue text-orange w-fit mx-auto py-1.24 px-3 rounded-3xl mb-6 ">You selected 4 out of 5</p>
+     <p className="bg-dark-blue text-orange w-fit mx-auto py-1.24 px-3 rounded-3xl mb-6 ">You selected {rateNote} out of 5</p>
 
      <h1 className="text-2xl font-bold mb-2.5">Thank You!</h1>
 
